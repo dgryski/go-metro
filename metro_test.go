66431c89d5c036f52583d1208970e43448073078
Damian Gryski <dgryski@gmail.com>
2016-05-30 08:57:30 +0200

    Update for new metrohash versions
    Fixes #1

--- metro_test.go
+++ metro_test.go
@@ -2,33 +2,26 @@ package metro
 
 import (
 	"encoding/binary"
-	"encoding/hex"
 	"testing"
 )
-
-// From src/testvector.h
 
 var key63 = []byte("012345678901234567890123456789012345678901234567890123456789012")
 
 func Test64(t *testing.T) {
 
 	tests := []struct {
-		f     func([]byte, uint32) uint64
-		which int
-		seed  uint32
-		want  string
+		seed uint64
+		want []byte
 	}{
-		{Hash64_1, 1, 0, "658F044F5C730E40"},
-		{Hash64_2, 2, 0, "073CAAB960623211"},
-		{Hash64_1, 1, 1, "AE49EBB0A856537B"},
-		{Hash64_2, 2, 1, "CF518E9CF58402C0"},
+
+		{0, []byte{0x6B, 0x75, 0x3D, 0xAE, 0x06, 0x70, 0x4B, 0xAD}},
+		{1, []byte{0x3B, 0x0D, 0x48, 0x1C, 0xF4, 0xB9, 0xB8, 0xDF}},
 	}
 
 	for _, tt := range tests {
-		want, _ := hex.DecodeString(tt.want)
-		want64 := binary.LittleEndian.Uint64(want)
-		if got := tt.f(key63, tt.seed); got != want64 {
-			t.Errorf("Hash64_%d(%q, %d)=%x, want %x\n", tt.which, key63, tt.seed, got, want64)
+		want := binary.LittleEndian.Uint64(tt.want)
+		if got := Hash64(key63, tt.seed); got != want {
+			t.Errorf("Hash64(%q, %d)=%x, want %x\n", key63, tt.seed, got, want)
 		}
 	}
 }
@@ -36,23 +29,18 @@ func Test64(t *testing.T) {
 func Test128(t *testing.T) {
 
 	tests := []struct {
-		f     func([]byte, uint32) (uint64, uint64)
-		which int
-		seed  uint32
-		want  string
+		seed uint64
+		want []byte
 	}{
-		{Hash128_1, 1, 0, "ED9997ED9D0A8B0FF3F266399477788F"},
-		{Hash128_2, 2, 0, "7BBA6FE119CF35D45507EDF3505359AB"},
-		{Hash128_1, 1, 1, "DDA6BA67F7DE755EFDF6BEABECCFD1F4"},
-		{Hash128_2, 2, 1, "2DA6AF149A5CDBC12B09DB0846D69EF0"},
+		{0, []byte{0xC7, 0x7C, 0xE2, 0xBF, 0xA4, 0xED, 0x9F, 0x9B, 0x05, 0x48, 0xB2, 0xAC, 0x50, 0x74, 0xA2, 0x97}},
+		{1, []byte{0x45, 0xA3, 0xCD, 0xB8, 0x38, 0x19, 0x9D, 0x7F, 0xBD, 0xD6, 0x8D, 0x86, 0x7A, 0x14, 0xEC, 0xEF}},
 	}
 
 	for _, tt := range tests {
-		want, _ := hex.DecodeString(tt.want)
-		want64a := binary.LittleEndian.Uint64(want)
-		want64b := binary.LittleEndian.Uint64(want[8:])
-		if gota, gotb := tt.f(key63, tt.seed); gota != want64a || gotb != want64b {
-			t.Errorf("Hash128_%d(%q, %d)=(%x, %x), want (%x, %x)\n", tt.which, key63, tt.seed, gota, gotb, want64a, want64b)
+		wanta := binary.LittleEndian.Uint64(tt.want)
+		wantb := binary.LittleEndian.Uint64(tt.want[8:])
+		if gota, gotb := Hash128(key63, tt.seed); gota != wanta || gotb != wantb {
+			t.Errorf("Hash128d(%q, %d)=(%x, %x), want (%x, %x)\n", key63, tt.seed, gota, gotb, wanta, wantb)
 		}
 	}
 }
