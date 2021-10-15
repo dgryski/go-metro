2661b20a2446686aa5f600171346ef08fc58e4b0
Damian Gryski <damian@gryski.com>
2021-10-15 15:16:34 -0700

    use more generic `purego` build tag

--- metro_stub.go
+++ metro_stub.go
@@ -1,5 +1,5 @@
-//go:build !noasm && amd64 && !gccgo && !tinygo
-// +build !noasm,amd64,!gccgo,!tinygo
+//go:build !noasm && amd64 && !gccgo && !purego
+// +build !noasm,amd64,!gccgo,!purego
 
 package metro
 
